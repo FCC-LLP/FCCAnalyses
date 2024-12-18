ae25b25d278caf7d037808844889093037b3a6b1
Lnura <ebakhish@cern.ch>
2024-12-18 15:44:47 +0100

    adjust shell files to create samples efficiently

--- examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/Condor_MadGraph/mg.sh
+++ examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/Condor_MadGraph/mg.sh
@@ -10,12 +10,31 @@ baseoutputdir="ALP_Z_aa_${mass_filename}GeV_cYY${coupling_filename}"
 
 # Define the EOS base directory
 eos_base_dir="/eos/user/e/ebakhish/MG/MGOutput"
+# afs_base_dir="/afs/cern.ch/user/e/ebakhish/FCCAnalyses/examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/Condor_MadGraph/MGOutput"
+
+#create directory for output
+if [ ! -d "/eos/user/e/ebakhish/MG/MGOutput/${baseoutputdir}" ]; then
+    mkdir -p "/eos/user/e/ebakhish/MG/MGOutput/${baseoutputdir}"  
+fi
+# if [ ! -d "${afs_base_dir}/${baseoutputdir}" ]; then
+#     mkdir -p "${afs_base_dir}/${baseoutputdir}"  
+# fi
 
 
-output_dir="${eos_base_dir}/${baseoutputdir}_${job_number}"
+output_dir="${eos_base_dir}/${baseoutputdir}/${baseoutputdir}_${job_number}"
+# output_dir="${afs_base_dir}/${baseoutputdir}/${baseoutputdir}_${job_number}"
+
+# if [ -d "$output_dir" ]; then
+#     counter=1
+#     while [ -d "${output_dir}_v${counter}" ]; do
+#         counter=$((counter + 1))
+#     done
+#     output_dir="${output_dir}_v${counter}"
+# fi
+
 temp_card="/eos/user/e/ebakhish/MG/Processcards/${baseoutputdir}_${job_number}.dat"
 
-
+# temp_card="/afs/cern.ch/user/e/ebakhish/FCCAnalyses/examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/Condor_MadGraph/Processcards/${baseoutputdir}_${job_number}.dat"
 input_file="/afs/cern.ch/user/e/ebakhish/FCCAnalyses/examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/Condor_MadGraph/mg_cards_collection/mg5_proc_card_ALP_Z_aa_template.dat"
 
 #random seed number for MG
--- examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/Condor_PythiaDelphes/runPythiaDelphesCondor.sh
+++ examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/Condor_PythiaDelphes/runPythiaDelphesCondor.sh
@@ -9,9 +9,11 @@ coupling_filename=$(echo ${coupling} | sed "s/\./p/g")
 
 baseoutputdir="ALP_Z_aa_${mass_filename}GeV_cYY${coupling_filename}"
 MGfiledir="${baseoutputdir}_${job_number}"
+#use this with correct number _v... if you are adding more samples afterwards
+#MGfiledir="${baseoutputdir}_${job_number}_v1"
 
 #input .lhe files for the pythia run
-inputfile="/eos/user/e/ebakhish/MG/MGOutput/$MGfiledir/Events/run_01/unweighted_events.lhe.gz"
+inputfile="/eos/user/e/ebakhish/MG/MGOutput/${baseoutputdir}/${MGfiledir}/Events/run_01/unweighted_events.lhe.gz"
 pythiacard=/afs/cern.ch/user/e/ebakhish/FCCAnalyses/examples/FCCee/bsm/LLPs/ALPs/ALP_sample_creation/ALP_pythia.cmnd
 temp_pythiacard="/eos/user/e/ebakhish/MG/Pythiacards/ALP_pythia_${MGfiledir}.cmnd"
 #outputfile= "/eos/user/e/ebakhish/MG/Pythia_Output/${MGfiledir}"
@@ -29,15 +31,10 @@ fi
 
 # only run following command if the MadGraph output was successful
 
-if [ -d "/eos/user/e/ebakhish/MG/MGOutput/$MGfiledir/Events/run_01" ]; then
+if [ -d "/eos/user/e/ebakhish/MG/MGOutput/${baseoutputdir}/${MGfiledir}/Events/run_01" ]; then
     # quick command to run DelphesPythia8_EDM4HEP
     # needs one input for the name of the output root-file
     DelphesPythia8_EDM4HEP /afs/cern.ch/user/e/ebakhish/FCC-config/FCCee/Delphes/card_IDEA.tcl /afs/cern.ch/user/e/ebakhish/FCC-config/FCCee/Delphes/edm4hep_IDEA.tcl $temp_pythiacard /eos/user/e/ebakhish/MG/Pythia_Output/${baseoutputdir}/${MGfiledir}.root
 else 
     echo "/$MGfiledir/Events/run_01 does not exist"
 fi
-
-
-# # quick command to run DelphesPythia8_EDM4HEP
-# # needs one input for the name of the output root-file
-# DelphesPythia8_EDM4HEP /afs/cern.ch/user/e/ebakhish/FCC-config/FCCee/Delphes/card_IDEA.tcl /afs/cern.ch/user/e/ebakhish/FCC-config/FCCee/Delphes/edm4hep_IDEA.tcl $temp_pythiacard /eos/user/e/ebakhish/MG/Pythia_Output/${baseoutputdir}/${MGfiledir}.root
